1fbdc3c7fd1fbe99fd2620d695179339c24e910f
lerickf9 <efuentes.edf9@gmail.com>
2025-08-04 01:41:26 -0400

    Clase y Metodos

--- src/POO/Automovil.java
+++ src/POO/Automovil.java
@@ -6,7 +6,38 @@ public class Automovil {
     String modelo;
     String color = "gris";
     double cilindrada;
+    int capacidadEstanque = 40;
 
+    public String verDetalle(){
+        return  "auto.fabricante = " + this.fabricante +
+                "\nauto.modelo = " + this.modelo +
+                "\nauto.color = " + this.color +
+                "\nauto.cilindrada = " + this.cilindrada;
 
+    }
+
+    public String acelerar (int rpm){
+        return "el auto " + this.fabricante + " acelerando a " + rpm + " rpm";
+    }
+
+    public String frenar(){
+        return this.fabricante + " " + this.modelo + " franando";
+
+    }
+
+    public String acelerarFrenar(int rpm){
+        String acelerar = this.acelerar(rpm);
+        String frenar = this.frenar();
+        return acelerar + "\n" + frenar;
+    }
+
+    public float calcularConsumo(int km, float porcentajeBencina){
+        return km/(capacidadEstanque * porcentajeBencina);
+    }
+
+    //Sobrecarga de metodo
+    public float calcularConsumo(int km, int porcentajeBencina){
+        return km/(capacidadEstanque * (porcentajeBencina/100f));
+    }
 
 }
--- src/POO/EjemploAutomovil.java
+++ src/POO/EjemploAutomovil.java
@@ -3,11 +3,11 @@ package POO;
 public class EjemploAutomovil {
     public static void main(String[] args) {
 
-        Automovil auto = new Automovil();
-        auto.fabricante = "Subaru";
-        auto.modelo = "Impreza";
-        auto.cilindrada = 2.0;
-        auto.color = "Blanco";
+        Automovil subaru = new Automovil();
+        subaru.fabricante = "Subaru";
+        subaru.modelo = "Impreza";
+        subaru.cilindrada = 2.0;
+        subaru.color = "Blanco";
 
         Automovil mazda = new Automovil();
         mazda.fabricante = "Mazda";
@@ -15,16 +15,14 @@ public class EjemploAutomovil {
         mazda.cilindrada = 3.0;
         mazda.color = "Rojo";
 
+        subaru.verDetalle();
+        mazda.verDetalle();
+        System.out.println(subaru.acelerar(3000));
 
-        System.out.println("auto.fabricante = " + auto.fabricante);
-        System.out.println("auto.modelo = " + auto.modelo);
-        System.out.println("auto.color = " + auto.color);
-        System.out.println("auto.cilindrada = " + auto.cilindrada);
+        System.out.println(mazda.acelerarFrenar(40000));
 
-        System.out.println("auto.fabricante = " + mazda.fabricante);
-        System.out.println("auto.modelo = " + mazda.modelo);
-        System.out.println("auto.color = " + mazda.color);
-        System.out.println("auto.cilindrada = " + mazda.cilindrada);
-        
+        System.out.println("Kilometros por litros " + subaru.calcularConsumo(300, 0.75f));
+        //Encapsulamiento
+        System.out.println("Kilometros por litros " + subaru.calcularConsumo(300, 75));
     }
 }
